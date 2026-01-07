d5dc2131b9dad4c8dab687a1beb64d40cf8a0a77
Rorozco11 <96450960+Rorozco11@users.noreply.github.com>
2026-01-06 20:48:21 -0500

    Fix ImportError: Replace langchain.tools.Tool with langchain_core.tools.StructuredTool

--- tools.py
+++ tools.py
@@ -1,12 +1,12 @@
 from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
-from langchain.tools import Tool
+from langchain_core.tools import StructuredTool
 from datetime import datetime
 
 
 search = DuckDuckGoSearchRun()
-search_tool = Tool(
-    name = "search",
-    func = search.run,
+search_tool = StructuredTool.from_function(
+    func=search.run,
+    name="search",
     description="Search the web for information"
 )
